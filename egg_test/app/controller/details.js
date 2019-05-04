e3271f9a73998d08066aa054bc93d385cc3788ce
yn22638 <1941263063@qq.com>
2019-05-04 17:13:28 +0800

    egg项目

---
+++ egg_test/app/controller/details.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class DetailsController extends Controller {
+    async index() {
+        const { ctx } = this;
+        await ctx.render("details.html", {
+            box: "米洛保险箱",
+            serial: "序列号",
+            number: "GFRGB44FDFD5FD",
+            Ip: "设备Ip",
+            Ip_number: "192.168.1.1",
+            Firmware_version: "固件版本",
+            Edition: "V2.01245",
+            state: "连接状态",
+            Yes: "已连接",
+        })
+
+    }
+}
+
+module.exports = DetailsController;
