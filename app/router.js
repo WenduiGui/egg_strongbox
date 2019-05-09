68bdce4a8c0128d70055c4a80c7359241b79b9fb
pandexin <1240526139>
2019-05-09 11:42:07 +0800

    改善

--- app/router.js
+++ app/router.js
@@ -8,6 +8,6 @@ module.exports = app => {
   router.get('/', controller.banner.index);
   router.get('/Associated', controller.associated.index);
   router.get('/state', controller.state.index);
-  router.get('/boxs', controller.boxs.index);
-
+  router.get('/open', controller.home1.open);
+  router.get('/recoed', controller.home1.recoed);
 };
