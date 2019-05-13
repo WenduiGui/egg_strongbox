31608705056f61ce2639151c0bbafd9f968ce4a9
yn22638 <1941263063@qq.com>
2019-05-13 17:25:42 +0800

    完善

--- app/controller/page.js
+++ app/controller/page.js
@@ -6,10 +6,11 @@ class AddController extends Controller {
     async index() {
         const { ctx } = this;
         await ctx.render("page.html", {
-            bt1: "智能保险箱",
-            bt:'MIKO保险箱',
-            sj:'最后一次位置:杭州',
-            sp5:"监听保险箱"
+            item: { 'bt1': '智能保险箱' },
+
+            bt: 'MIKO保险箱',
+            sj: '最后一次位置：杭州',
+            sp5: "监听保险箱"
         })
     }
 }
